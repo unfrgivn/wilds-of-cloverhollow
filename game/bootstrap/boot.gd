e5fa5914c71992bf490b5cc0862e54b326d63332
unfrgivn <bradley.ash@gmail.com>
2026-01-19 14:40:41 -0600

    Set default spawn to Cloverhollow town

--- game/bootstrap/boot.gd
+++ game/bootstrap/boot.gd
@@ -1,6 +1,6 @@
 extends Node
 
-const DEFAULT_SCENE = "res://game/scenes/tests/TestRoom_Movement.tscn"
+const DEFAULT_SCENE = "res://game/scenes/areas/cloverhollow/Area_Cloverhollow_Town.tscn"
 
 func _ready() -> void:
 	get_tree().call_deferred("change_scene_to_file", DEFAULT_SCENE)
